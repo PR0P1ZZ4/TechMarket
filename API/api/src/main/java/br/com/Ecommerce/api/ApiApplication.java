1f563541b5613046d133044c5e1483272ead3b41
fic <fic@NOTE06-S28>
2025-09-02 21:55:05 -0300

    Novo API

---
+++ API/api/src/main/java/br/com/Ecommerce/api/ApiApplication.java
@@ -0,0 +1,47 @@
+package br.com.Ecommerce.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+
+
+	public static void main(String[] args) {
+
+
+
+        System.out.println("Samuel");
+
+
+
+		SpringApplication.run(ApiApplication.class, args);
+
+
+
+
+
+
+
+
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
